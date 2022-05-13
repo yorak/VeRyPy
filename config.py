299c58187478e4571b0cd5a07f4435c09fdc7d59
Jussi Rasku <jussi.rasku@gmail.com>
2022-05-13 10:47:22 +0300

    Fix HOME missing in Windows
    A cross-platform solution for both py 2.7 and 3.X is to use os.path.expanduser

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ Created on Thu Aug 10 15:43:09 2017
 @author: juherask
 """
 
-import os
+from os import path
 
 DEBUG_VERBOSITY = 3
 
@@ -17,11 +17,14 @@ MAX_MIP_SOLVER_RUNTIME = 60*10 # 10m
 
 MIP_SOLVER_THREADS = 1 # 0 is automatic (parallel computing)
 
-# venv does not allow use of ~ for some reason in paths on Ubuntu 20.04.
-BENCHMARKS_BASEPATH = os.path.join(os.environ["HOME"], r"Projects/Research/VRPBenchmarks")
+# Set up some paths where to find benchmarks and external solvers
 
-LKH_EXE_PATH = os.path.join(os.environ["HOME"], r"Projects/Research/TSP/LKH-2.0.9/LKH")
+HOME_PATH = expanduser("~")
+
+BENCHMARKS_BASEPATH = path.join(HOME_PATH, r"Research/VRPBenchmarks")
+
+LKH_EXE_PATH = path.join(HOME_PATH, r"Research/TSP/LKH-2.0.9/LKH")
 LKH_EXACT_DISTANCES_PRECISION_DECIMALS = 1000.0 # of the form 0.123 
 
-ACOTSP_EXE_PATH = os.path.join(os.environ["HOME"], r"Projects/Research/TSP/ACOTSP-master/acotsp")
+ACOTSP_EXE_PATH = path.join(HOME_PATH, r"Research/TSP/ACOTSP-master/acotsp")
 ACOTSP_EXACT_DISTANCES_PRECISION_DECIMALS = 1000.0 # of the form 0.123 
