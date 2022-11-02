3f95998b8d2e7820508ffda06e5d7e835cf70070
yorak <jussi.rasku@gmail.com>
2022-11-02 16:13:35 +0200

    More sensible defaults

--- verypy/config.py
+++ verypy/config.py
@@ -5,7 +5,8 @@ Created on Thu Aug 10 15:43:09 2017
 @author: juherask
 """
 
-from os import path
+from os import path, name
+exe_ext = ".exe" if name == 'nt' else ""
 
 DEBUG_VERBOSITY = 3
 
@@ -18,13 +19,14 @@ MAX_MIP_SOLVER_RUNTIME = 60*10 # 10m
 MIP_SOLVER_THREADS = 1 # 0 is automatic (parallel computing)
 
 # Set up some paths where to find benchmarks and external solvers
-
 HOME_PATH = path.expanduser("~")
 
 BENCHMARKS_BASEPATH = path.join(HOME_PATH, r"Research/VRPBenchmarks")
 
-LKH_EXE_PATH = path.join(HOME_PATH, r"Research/TSP/LKH-2.0.9/LKH")
+LKH_EXE_PATH = path.join(HOME_PATH, r"Research/TSP/LKH-2.0.9/lkh"+exe_ext)
 LKH_EXACT_DISTANCES_PRECISION_DECIMALS = 1000.0 # of the form 0.123 
+print(LKH_EXE_PATH)
 
-ACOTSP_EXE_PATH = path.join(HOME_PATH, r"Research/TSP/ACOTSP-master/acotsp")
+ACOTSP_EXE_PATH = path.join(HOME_PATH, r"Research/TSP/ACOTSP-1.03-ls/acotsp"+exe_ext)
 ACOTSP_EXACT_DISTANCES_PRECISION_DECIMALS = 1000.0 # of the form 0.123 
+print(ACOTSP_EXE_PATH)
